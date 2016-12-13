e99766b80dcecc464a73c347b4c0ddc5d37ed404
Charles JUDITH <c.judith@criteo.com>
2016-12-13 07:29:37 +0100

    Remove useless package and add CentOS 7 support

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,42 +12,26 @@ default['centreon_engine']['user'] = 'centreon-engine'
 default['centreon_engine']['group'] = 'centreon-engine'
 
 # --[ Packages ]--
-default['centreon_engine']['server']['packages'] = %w{centreon-engine nagios-plugins nrpe-plugin}
+default['centreon_engine']['server']['packages'] = 'centreon-engine'
 
 # --[ Repository configuration ]--
 case node['platform']
 when 'redhat', 'centos'
-  default['centreon_engine']['repositories'] = %w{ces-standard ces-standard-noarch ces-standard-deps ces-standard-deps-noarch}
+  default['centreon_engine']['repositories'] = %w{ces-standard ces-standard-noarch}
 
   default['centreon_engine']['ces-standard']['repositoryid'] = 'ces-standard'
   default['centreon_engine']['ces-standard']['description'] = 'Centreon RPM repository for ces $releasever'
-  default['centreon_engine']['ces-standard']['baseurl'] = 'http://yum.centreon.com/standard/3.0/stable/$basearch/'
+  default['centreon_engine']['ces-standard']['baseurl'] = "http://yum.centreon.com/standard/3.4/el$releasever/stable/$basearch/"
   default['centreon_engine']['ces-standard']['gpgcheck'] = true
   default['centreon_engine']['ces-standard']['enabled'] = true
-  default['centreon_engine']['ces-standard']['gpgkey'] = 'http://yum.centreon.com/standard/3.0/stable/RPM-GPG-KEY-CES'
+  default['centreon_engine']['ces-standard']['gpgkey'] = "http://yum.centreon.com/standard/3.4/el$releasever/stable/RPM-GPG-KEY-CES"
   default['centreon_engine']['ces-standard']['managed'] = true
 
   default['centreon_engine']['ces-standard-noarch']['repositoryid'] = 'ces-standard-noarch'
   default['centreon_engine']['ces-standard-noarch']['description'] = 'Centreon RPM repository for ces $releasever'
-  default['centreon_engine']['ces-standard-noarch']['baseurl'] = 'http://yum.centreon.com/standard/3.0/stable/noarch/'
+  default['centreon_engine']['ces-standard-noarch']['baseurl'] = "http://yum.centreon.com/standard/3.4/el$releasever/stable/noarch/"
   default['centreon_engine']['ces-standard-noarch']['gpgcheck'] = true
   default['centreon_engine']['ces-standard-noarch']['enabled'] = true
-  default['centreon_engine']['ces-standard-noarch']['gpgkey'] = 'http://yum.centreon.com/standard/3.0/stable/RPM-GPG-KEY-CES'
+  default['centreon_engine']['ces-standard-noarch']['gpgkey'] = "http://yum.centreon.com/standard/3.4/el$releasever/stable/RPM-GPG-KEY-CES"
   default['centreon_engine']['ces-standard-noarch']['managed'] = true
-
-  default['centreon_engine']['ces-standard-deps']['repositoryid'] = 'ces-standard-deps'
-  default['centreon_engine']['ces-standard-deps']['description'] = 'Centreon dependencies RPM repository for ces $releasever'
-  default['centreon_engine']['ces-standard-deps']['baseurl'] = 'http://yum.centreon.com/standard/3.0/stable/dependencies/$basearch/'
-  default['centreon_engine']['ces-standard-deps']['gpgcheck'] = true
-  default['centreon_engine']['ces-standard-deps']['enabled'] = true
-  default['centreon_engine']['ces-standard-deps']['gpgkey'] = 'http://yum.centreon.com/standard/3.0/stable/RPM-GPG-KEY-CES'
-  default['centreon_engine']['ces-standard-deps']['managed'] = true
-
-  default['centreon_engine']['ces-standard-deps-noarch']['repositoryid'] = 'ces-standard-deps-noarch'
-  default['centreon_engine']['ces-standard-deps-noarch']['description'] = 'Centreon dependencies RPM repository for ces $releasever'
-  default['centreon_engine']['ces-standard-deps-noarch']['baseurl'] = 'http://yum.centreon.com/standard/3.0/stable/dependencies/noarch/'
-  default['centreon_engine']['ces-standard-deps-noarch']['gpgcheck'] = true
-  default['centreon_engine']['ces-standard-deps-noarch']['enabled'] = true
-  default['centreon_engine']['ces-standard-deps-noarch']['gpgkey'] = 'http://yum.centreon.com/standard/3.0/stable/RPM-GPG-KEY-CES'
-  default['centreon_engine']['ces-standard-deps-noarch']['managed'] = true
 end
